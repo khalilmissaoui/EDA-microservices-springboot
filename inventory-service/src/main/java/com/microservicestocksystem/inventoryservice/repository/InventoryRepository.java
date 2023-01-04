48186583ca166147de82313da31cc474deafce24
khalil <khalil.missaoui1@esprit.tn>
2023-01-04 13:20:59 +0100

    add logic to inventory service

--- inventory-service/src/main/java/com/microservicestocksystem/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/microservicestocksystem/inventoryservice/repository/InventoryRepository.java
@@ -3,8 +3,9 @@ package com.microservicestocksystem.inventoryservice.repository;
 
 import com.microservicestocksystem.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
+    Optional<Inventory> findBySkuCode(String skuCode);
 }
