e3bf28797185739d4c28b607967ed5089ace40c0
khalil <khalil.missaoui1@esprit.tn>
2023-01-05 18:38:31 +0100

    WIP -- fix the containers logic flow

--- inventory-service/src/main/java/com/microservicestocksystem/inventoryservice/model/Inventory.java
+++ inventory-service/src/main/java/com/microservicestocksystem/inventoryservice/model/Inventory.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "t_inventory")
@@ -13,7 +14,9 @@ import javax.persistence.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Inventory {
+public class Inventory implements Serializable {
+    private static final long serialVersionUID = 1L;
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
