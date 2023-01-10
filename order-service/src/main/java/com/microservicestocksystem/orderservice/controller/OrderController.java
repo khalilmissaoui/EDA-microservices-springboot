8c6d1e734af56abfeefbf28ff912afa2939e1e87
khalil <khalil.missaoui1@esprit.tn>
2023-01-11 00:26:44 +0100

    add circuit breaker to solve sync communication failing between microservices

--- order-service/src/main/java/com/microservicestocksystem/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/microservicestocksystem/orderservice/controller/OrderController.java
@@ -3,9 +3,12 @@ package com.microservicestocksystem.orderservice.controller;
 
 import com.microservicestocksystem.orderservice.dto.OrderRequest;
 import com.microservicestocksystem.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -13,11 +16,15 @@ import org.springframework.web.bind.annotation.*;
 public class OrderController {
 
     private final OrderService orderService;
-
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please order after some time!");
     }
 }
